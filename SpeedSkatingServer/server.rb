e9b8e713a06191ac3c3c3584381ea1e0f34df24c
Jurre Stender <jurrestender@hotmail.com>
2012-09-17 10:42:05 +0200

    print local ip addr on startup

--- SpeedSkatingServer/server.rb
+++ SpeedSkatingServer/server.rb
@@ -4,6 +4,19 @@ require 'csv'
 Encoding.default_external = Encoding::UTF_8
 Encoding.default_internal = Encoding::UTF_8
 
+
+def local_ip
+  orig = Socket.do_not_reverse_lookup  
+  Socket.do_not_reverse_lookup =true # turn off reverse DNS resolution temporarily
+  UDPSocket.open do |s|
+    s.connect '64.233.187.99', 1 #google
+    s.addr.last
+  end
+ensure
+  Socket.do_not_reverse_lookup = orig
+end
+
+
 port = 2000
 
 if ARGV.length > 0
@@ -11,16 +24,17 @@ if ARGV.length > 0
 end
 
 server = TCPServer.open(port) 
-puts "server started"
+puts "server started \nlistening on #{local_ip}"
+
 loop do
  Thread.start(server.accept) do |client|
    puts "client connected"
    count = 0
     CSV.foreach("KramerOlympics10000.csv") do |row|
+      sleep(2)
     	unless count < 2
 	      puts row.join
 	    	client.puts(row.join + "\n")
-	    	sleep(1)
 	    end
     	count += 1
     end
