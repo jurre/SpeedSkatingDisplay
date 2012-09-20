925f29373579ca2f071afb521b5f4cade68aa474
Jurre Stender <jurrestender@hotmail.com>
2012-09-20 10:22:45 +0200

    add schedule class

---
+++ src/com/example/Schedule.java
@@ -0,0 +1,15 @@
+package com.example;
+
+import java.util.LinkedList;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: jurrestender
+ * Date: 9/20/12
+ * Time: 10:20 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class Schedule {
+    private LinkedList<LapData> lapDatas;
+    private String name;
+}
