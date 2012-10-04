9828175eb298933958a5a4a5d68fdb65df8b8b65
Jurre Stender <jurrestender@hotmail.com>
2012-10-04 13:52:48 +0200

    merge fix 2

--- SpeedSkatingServer/server.rb
+++ SpeedSkatingServer/server.rb
@@ -62,7 +62,6 @@ def accept_connections(clients_array, server)
     loop do
       Thread.start(server.accept) do |client|
         puts "client connected"
-        client.puts @last_message unless @last_message.nil? # send the last lapdata as soon as the client connects
         client.puts @last_message unless @last_message.nil? # send the last lapData as soon as the client connects
         clients_array << client
       end
